4977b0932df78d013b16f681b98bda6d129e9880
Kim Hammar <kimham@kth.se>
2019-07-08 22:42:14 +0200

    Add settings endpoint for feature store

--- hopsworks-web/yo/app/scripts/services/ModalService.js
+++ hopsworks-web/yo/app/scripts/services/ModalService.js
@@ -1349,7 +1349,7 @@ angular.module('hopsWorksApp')
                 });
                 return modalInstance.result;
             },
-            viewFeatureInfo: function (size, projectId, feature, featurestore) {
+            viewFeatureInfo: function (size, projectId, feature, featurestore, settings) {
                 var modalInstance = $uibModal.open({
                     templateUrl: 'views/featureViewInfo.html',
                     controller: 'featureViewInfoCtrl as featureViewInfoCtrl',
@@ -1374,12 +1374,15 @@ angular.module('hopsWorksApp')
                         },
                         featurestore: function () {
                             return featurestore;
+                        },
+                        settings: function () {
+                            return settings;
                         }
                     }
                 });
                 return modalInstance.result;
             },
-            viewFeaturegroupInfo: function (size, projectId, featuregroup, featurestore) {
+            viewFeaturegroupInfo: function (size, projectId, featuregroup, featurestore, settings) {
                 var modalInstance = $uibModal.open({
                     templateUrl: 'views/featuregroupViewInfo.html',
                     controller: 'featuregroupViewInfoCtrl as featuregroupViewInfoCtrl',
@@ -1403,6 +1406,9 @@ angular.module('hopsWorksApp')
                             return featuregroup;
                         },
                         featurestore: function () {
+                            return featurestore;
+                        },
+                        settings: function () {
                             return featurestore;
                         }
                     }
@@ -1439,7 +1445,7 @@ angular.module('hopsWorksApp')
                 });
                 return modalInstance.result;
             },
-            selectFeatureType: function (size) {
+            selectFeatureType: function (size, settings) {
                 var modalInstance = $uibModal.open({
                     templateUrl: 'views/selectFeatureType.html',
                     controller: 'selectFeatureTypeCtrl as selectFeatureTypeCtrl',
@@ -1456,6 +1462,9 @@ angular.module('hopsWorksApp')
                                         return $q.reject(err);
                                     });
                             }],
+                        settings: function () {
+                            return settings;
+                        },
                     }
                 });
                 return modalInstance.result;
